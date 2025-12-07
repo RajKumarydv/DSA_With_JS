5b3ec11440283d8f3099fee94ed2fa8f13b5f1c4
Raj <rajk70796@gmail.com>
2025-12-07 23:22:45 +0530

    Add implementation for maximum sum subarray of size K using sliding window technique

---
+++ q8-slidingwindow.js
@@ -0,0 +1,23 @@
+function maxSumSubarray(arr,k){
+  // Q8: Maximum Sum Subarray of Size K
+  // 🔹 Explanation
+  // Sliding window technique ka use karo.
+  // 🔹 Example
+  // Input: [2, 1, 5, 1, 3, 2], k=3
+  // Output: 9
+
+
+ let sum = 0;
+ for(let i = 0;i<k;i++){
+    sum = sum+arr[i];
+ }
+ let mexSum = sum ;
+ for(let i = k; i<arr.length;i++){
+  sum = sum +arr[i] -arr[i-k];
+  if(sum>mexSum){
+    mexSum = sum;
+   }
+ }
+  return mexSum;
+ }
+  console.log(maxSumSubarray([2, 1, 5, 1, 3, 2],3));
